755e92af09673650faa0faa58508ffa556422dbd
C-DGameMaker <charlotte5634@outlook.com>
2026-03-03 11:03:50 -0400

    Can move

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using NUnit.Framework.Constraints;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float _movementSpeed = 5;
+    [SerializeField] Vector2 _moveInput;
+
+    private Rigidbody _playerRigidBody;
+
+    private void Awake()
+    {
+        _playerRigidBody = GetComponent<Rigidbody>();
+
+        if (_playerRigidBody == null) Debug.LogError("RigidBody not founded");
+    }
+
+    private void FixedUpdate()
+    {
+        HandlePlayerMovement();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _moveInput = context.ReadValue<Vector2>();
+    }
+    public void OnJump()
+    {
+
+    }
+
+    private void HandlePlayerMovement()
+    {
+        Vector3 move = new Vector3(_moveInput.x, 0, _moveInput.y) * _movementSpeed * Time.deltaTime;
+        _playerRigidBody.MovePosition(_playerRigidBody.position + move);
+    }
+}
