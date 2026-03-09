55fe939074d202886dcef81543befcbde54225ab
C-DGameMaker <charlotte5634@outlook.com>
2026-03-09 17:14:01 -0300

    Moving Platforms Works

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -1,10 +1,20 @@
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
     List<Vector3> _wayPoints = new List<Vector3>();
+    [SerializeField] float _movementSpeed;
+
+    private Vector3 _currentPos;
+    private Vector3 _newPos;
+
+    private bool _isReverse;
+    private int _nextPosIndex;
+
+    private float _movementTimer;
 
     private void Start()
     {
@@ -13,7 +23,70 @@ public class MovingPlatform : MonoBehaviour
             if (!child.CompareTag("Waypoint")) continue;
             _wayPoints.Add(child.position);
         }
+
+        _currentPos = _wayPoints[0];
+        _newPos = _wayPoints[1];
+        _nextPosIndex = 1;
+        _isReverse = false;
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            other.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+        }
+    }
+
+    private void Update()
+    {
+        _movementTimer += Time.deltaTime / _movementSpeed;
+        _movementTimer = Mathf.Min(_movementTimer, 1);
+        transform.position = Vector3.Lerp(_currentPos, _newPos, _movementTimer);
+
+        if(Vector3.Distance(transform.position, _newPos) < 0.2)
+        {
+            _movementTimer = 0;
+            if (_nextPosIndex >= _wayPoints.Count - 1)
+            {
+                _isReverse = true;
+            }
+            if (_nextPosIndex <= 0)
+            {
+                _isReverse = false;
+            }
+
+            if (_isReverse == false)
+            {
+                _nextPosIndex++;
+                _currentPos = _wayPoints[_nextPosIndex - 1];
+                _newPos = _wayPoints[_nextPosIndex];
+                
+            }
+            else if (_isReverse != false)
+            {
+                
+                _nextPosIndex--;
+                _currentPos = _wayPoints[_nextPosIndex + 1];
+                _newPos = _wayPoints[_nextPosIndex];
+                
+            }
+        }
+        
+    }
+
+    
+
+
+
     private void OnDrawGizmos()
     {
         if (Application.isPlaying) return;
