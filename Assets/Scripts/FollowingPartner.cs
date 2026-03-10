71673dc51dc489eb988fd83f486bd687da049654
C-DGameMaker <charlotte5634@outlook.com>
2026-03-10 11:19:36 -0300

    Follower shoves player around
    Follower is not meant to shove player around
    
    Charlie is gonna lose a lot of hair because of this, I shall never recover

---
+++ Assets/Scripts/FollowingPartner.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FollowingPartner : MonoBehaviour
+{
+    [SerializeField] GameObject _player;
+    [SerializeField] float _movementSpeed;
+    private Rigidbody _followerRigidbody;
+
+    private void Start()
+    {
+        if(_player == null)
+        {
+            Debug.Log("Add player to follow");
+        }
+
+        _followerRigidbody = GetComponent<Rigidbody>();
+        if(_followerRigidbody == null)
+        {
+            Debug.Log("WHERE IS THE RIGIDBODY MICHAEL?! WHERE IS IT?! WHAT DID YOU DO TO IT MICHAEL");
+        }
+    }
+
+    private void Update()
+    {
+        Vector3 lookDirection = (_player.transform.position - transform.position).normalized;
+
+        _followerRigidbody.AddForce(lookDirection * _movementSpeed);
+    }
+}
