70130f9a6bc5a565ba6a99c184cdaa762534f1ca
C-DGameMaker <charlotte5634@outlook.com>
2026-04-09 18:31:54 -0300

    Deleted Script that didn't do anything

--- Assets/Prefabs/BuyButton.cs
+++ Assets/Prefabs/BuyButton.cs
@@ -4,28 +4,7 @@ using UnityEngine;
 
 public class BuyButton : MonoBehaviour
 {
-    [SerializeField] int price;
-    [SerializeField] GameplayUIManager coinCount;
-    [SerializeField] TextMeshProUGUI text;
+    
 
-    private UIManager _uiManager;
-    private PlayerController _playerController;
-    void Start()
-    {
-        
-    }
-
-    public void ButtonToBuy()
-    {
-        if (coinCount._count >= price)
-        {
-            text.text = "Thank you for purchasing";
-            _playerController._playerHealth.IncreaseMaxHealth(5);
-            coinCount.SubtractCount(10);
-        }
-        else
-        {
-            text.text = "You don't have enough to buy.";
-        }
-    }
+    
 }
